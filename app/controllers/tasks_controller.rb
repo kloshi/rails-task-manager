58adce05c785ab7af7f3e1b63d937517020aed83
Andrzej Gbur <andrzej.gbur@yahoo.com>
2018-10-30 16:00:51 +0100

    almost finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,44 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  # i need a corresponding view here
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  # here I need a view
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  # here no view is needed - it's just a task in the controller
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,24 @@ Rails.application.routes.draw do
   # in the Task app:
 
   # read all tasks
-  get 'tasks', to: 'restaurants#index'
+  # get 'tasks', to: 'tasks#index'
 
   # create one task (2 requests: 1.get the form to enter the details; 2. post the params and )
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
 
   # read one selected task
-  get 'tasks/:id', to: 'tasks#show'
+  # get 'tasks/:id', to: 'tasks#show'
   # GET tasks/12
   # params[:id] will be 12 (12 will be intercepted from the url)
 
   # update a task (2 requests: 1. get the edit form; 2. post(patch!) the params and update in the DB)
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
 
   # delete a task
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
   # all the 7 routes may be replaced:
-  # resources :tasks
+  resources :tasks
 end
